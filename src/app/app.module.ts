4a19a93f75b98fa325e8a2a4129b83b27e746318
jerem5962 <jerem-62@outlook.com>
2021-01-26 17:10:11 +0100

    feature: Ajout du menu déroulant par glisser/glisser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-navbar',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {}
-
+  
+  openMenu() {
+    this.menu.enable(true, "first")
+    this.menu.open('first')
+  }
 }
